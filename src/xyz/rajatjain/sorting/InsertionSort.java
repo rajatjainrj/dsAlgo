313dc0b25d4d05bcbfc68a4c306939a1b23e567e
Rajat Jain <rajatjain@Rajats-MacBook-Pro.local>
2021-12-06 00:18:49 +0530

    Added Insertion Sort implementation

--- src/xyz/rajatjain/sorting/InsertionSort.java
+++ src/xyz/rajatjain/sorting/InsertionSort.java
@@ -1,10 +1,37 @@
 package xyz.rajatjain.sorting;
 
-public class InsertionSort extends Sort{
+public class InsertionSort extends Sort {
 
     @Override
     public void sortValues(int[] array, boolean ascending) {
+        setArrayAndAscending(array, ascending);
+        for (int currentIndex = 1; currentIndex < array.length; currentIndex++) {
+            int currentElement = array[currentIndex];
+            int lastSortedIndex = currentIndex - 1;
+            if (ascending) {
+                while (lastSortedIndex > -1 && array[lastSortedIndex] > currentElement) {
+                    array[lastSortedIndex + 1] = array[lastSortedIndex];
+                    lastSortedIndex--;
+                }
+            } else {
+                while (lastSortedIndex > -1 && array[lastSortedIndex] < currentElement) {
+                    array[lastSortedIndex + 1] = array[lastSortedIndex];
+                    lastSortedIndex--;
+                }
+            }
+            array[lastSortedIndex + 1] = currentElement;
+            printValues();
+        }
+    }
 
+    public static void main(String[] args) {
+        InsertionSort insertionSort = new InsertionSort();
+        insertionSort.insertionSortImplementation();
+    }
+
+    private void insertionSortImplementation() {
+        insertValues();
+        sortValues();
     }
 
 }
--- src/xyz/rajatjain/sorting/Sort.java
+++ src/xyz/rajatjain/sorting/Sort.java
@@ -9,8 +9,6 @@ public abstract class Sort {
 
     int[] array;
 
-    boolean status = true;
-
     boolean ascending = true;
 
     public void insertValues() {
