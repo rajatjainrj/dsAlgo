fdb2b8ac4df5f2378565416fd22cd2f00b0c2919
Rajat Jain <rajat.jain@techchefz.com>
2021-12-18 15:28:14 +0530

    Optimizations

--- src/xyz/rajatjain/data/structures/Stack.java
+++ src/xyz/rajatjain/data/structures/Stack.java
@@ -3,13 +3,13 @@ package xyz.rajatjain.data.structures;
 public class Stack {
 
     int MAX_SIZE;
-    int currentPointer;
+    int TOP_OF_STACK;
     int[] stack;
 
     public Stack(int MAX_SIZE) {
         this.MAX_SIZE = MAX_SIZE;
         stack = new int[MAX_SIZE];
-        currentPointer = -1;
+        TOP_OF_STACK = -1;
     }
 
     public Stack() {
@@ -17,14 +17,14 @@ public class Stack {
     }
 
     public boolean isFull() {
-        if (currentPointer >= MAX_SIZE - 1) {
+        if (TOP_OF_STACK >= MAX_SIZE - 1) {
             return true;
         }
         return false;
     }
 
     public boolean isEmpty() {
-        if (currentPointer < 0) {
+        if (TOP_OF_STACK < 0) {
             return true;
         }
         return false;
@@ -35,7 +35,7 @@ public class Stack {
             System.out.println("Stack Overflow");
             return false;
         } else {
-            stack[++currentPointer] = item;
+            stack[++TOP_OF_STACK] = item;
             System.out.println(item + " Pushed into Stack");
             return true;
         }
@@ -44,9 +44,9 @@ public class Stack {
     public int pop() {
         if (isEmpty()) {
             System.out.println("Stack Underflow");
-            return 0;
+            return -1;
         } else {
-            int item = stack[currentPointer--];
+            int item = stack[TOP_OF_STACK--];
             System.out.println(item + " popped from Stack");
             return item;
         }
@@ -57,17 +57,17 @@ public class Stack {
             System.out.println("Stack Underflow");
             return 0;
         } else {
-            return stack[currentPointer];
+            return stack[TOP_OF_STACK];
         }
     }
 
     public void printStack() {
-        if(currentPointer == -1){
+        if (TOP_OF_STACK == -1) {
             System.out.println("Empty Stack - []");
             return;
         }
         System.out.print("[");
-        for (int i = 0; i <= currentPointer; i++) {
+        for (int i = 0; i <= TOP_OF_STACK; i++) {
             System.out.print(stack[i] + " ");
         }
         System.out.println("]");
@@ -119,11 +119,12 @@ public class Stack {
         }
 
         int min = stack[0];
-        for (int i = 0; i <= currentPointer; i++) {
+        for (int i = 0; i <= TOP_OF_STACK; i++) {
             if(min > stack[i]){
                 min = stack[i];
             }
         }
         return min;
     }
+
 }
